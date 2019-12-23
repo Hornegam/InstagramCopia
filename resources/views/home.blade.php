43508a31ad065b670861425827aed1cdb3054216
Chico <22759@pmc.local>
2019-12-23 12:53:38 -0300

    Commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 p-4 mx-auto">
+            <img src="/svg/menia.jpg" style="max-height: 200px" class="rounded-circle pr-10 mx-auto">
+        </div>
+        <div class="col-md-9 pt-5 pl-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user ->username}}</h1>
+                <a href="#">Novo Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>10</strong> posts</div>
+                <div class="pr-3"><strong>23k</strong> seguidores</div>
+                <div class="pr-3"><strong>212</strong> seguindo</div>
+            </div>
+            <div class="pt-3 font-weight-bold">
+                {{ $user-> profile-> title}}
+            </div>
+            <div>{{ $user -> profile -> description}}</div>
+            <div><a href="#"><strong>{{$user -> profile -> url}}</strong></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="\svg\cachorro.jpg" class="w-100 h-100">
+        </div>
+        <div class="col-4">
+            <img src="\svg\group-photo.png" class="w-100 h-100">
+        </div>
+        <div class="col-4">
+            <img src="\svg\download.jpg" class="w-100 h-100">
+        </div>
+    </div>
+    <div class="row"></div>
+</div>
+@endsection
